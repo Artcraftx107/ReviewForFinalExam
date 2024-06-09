4ae6eca52c9c07f4ed734472d203e2d7f26c41b2
Artcraft <arturoao@uma.es>
2024-06-09 23:14:36 +0200

    Refactored Store.java class

--- prOnlineShop/src/main/java/shop/Store.java
+++ prOnlineShop/src/main/java/shop/Store.java
@@ -11,125 +11,102 @@ public class Store {
     private List<Order> orders;
 
     public Store(){
-        this.products=new ArrayList<>();
-        this.customers=new ArrayList<>();
-        this.orders=new ArrayList<>();
+        this.products = new ArrayList<>();
+        this.customers = new ArrayList<>();
+        this.orders = new ArrayList<>();
     }
 
-    public void addProduct(Product product, int addedStock){
-        if(product!=null){
-            if(products.contains(product)){
-                products.set(products.indexOf(product), product);
-                product.addStock(addedStock);
-            }else{
-                products.add(product);
-                product.addStock(addedStock);
+    public void addProduct(Product product, int addedStock) {
+        if (product == null) {
+            throw new IllegalArgumentException("Product cannot be null");
+        }
+        Product existingProduct = searchProductById(product.getProductId());
+        if (existingProduct != null) {
+            existingProduct.addStock(addedStock);
+        } else {
+            product.addStock(addedStock);
+            products.add(product);
+        }
+    }
+
+    public void removeProduct(Product product, int removedStock) {
+        if (product == null) {
+            throw new IllegalArgumentException("Product cannot be null");
+        }
+        Product existingProduct = searchProductById(product.getProductId());
+        if (existingProduct != null && removedStock > 0 && removedStock <= existingProduct.getStockQuantity()) {
+            existingProduct.removeStock(removedStock);
+            if (existingProduct.getStockQuantity() == 0) {
+                products.remove(existingProduct);
             }
-        }else{
-            throw new IllegalArgumentException("The product cannot be null");
+        } else {
+            System.err.println("The product specified isn't in the list or the amount of stock to be removed is not valid");
         }
     }
 
-    public void removeProduct(Product product, int removedStock){
-        if(product!=null){
-            if(products.contains(product) && removedStock>0 && removedStock<products.get(products.indexOf(product)).getStockQuantity()){
-                products.get(products.indexOf(product)).removeStock(removedStock);
-                products.remove(product);
-            }else {
-                System.err.println("The product specified isn't in the list or the amount of stock to be removed is not valid");
-            }
-        }else{
-            throw new IllegalArgumentException("The product cannot be null");
+    public void updateProduct(Product updatedProduct) {
+        Product existingProduct = searchProductById(updatedProduct.getProductId());
+        if (existingProduct != null) {
+            products.set(products.indexOf(existingProduct), updatedProduct);
+        } else {
+            throw new NoSuchElementException("The product isn't in the list of added products");
         }
     }
 
-    public void updateProduct(Product updatedProduct){
-        int pos = searchProductByName(updatedProduct.getName());
-        if(pos!=-1){
-            products.set(products.indexOf(pos), updatedProduct);
-        }else{
-            throw new NoSuchElementException("The product isnt in the list of added products");
-        }
-    }
-
-    public int searchProductByName(String name){
-        int pos = 0;
-        boolean found = false;
-        while(pos<products.size()&&!found){
-            if(products.get(pos).getName().equalsIgnoreCase(name)){
-                found=true;
-            }else{
-                pos++;
+    public int searchProductByName(String name) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getName().equalsIgnoreCase(name)) {
+                return i;
             }
         }
-        if(!found){
-            return -1;
-        }else{
-            return pos;
-        }
+        return -1;
     }
 
-    public Product searchProductbyID(int productID){
-        int pos = 0;
-        Product aux = new Product(0, "n", "d", 0, 0);
-        boolean found = false;
-        while(pos<products.size()&&!found){
-            if(products.get(pos).getProductId()==productID){
-                found=true;
-                aux=products.get(pos);
-            }else{
-                pos++;
+    public Product searchProductById(int productId) {
+        for (Product product : products) {
+            if (product.getProductId() == productId) {
+                return product;
             }
         }
-        if(!found){
-            aux = null;
-        }
-
-        return aux;
+        return null;
     }
 
-    public void addCustomer(Customer customer){
-        if(customer!=null){
+    public void addCustomer(Customer customer) {
+        if (customer != null) {
             customers.add(customer);
-        }else{
+        } else {
             throw new IllegalArgumentException("Customer cannot be null");
         }
     }
 
-    public void removeCustomer(int customerID){
-        Customer customer = getCostumerById(customerID);
-        if (customer!=null){
+    public void removeCustomer(int customerId) {
+        Customer customer = getCustomerById(customerId);
+        if (customer != null) {
             customers.remove(customer);
-        }else{
-            throw new NoSuchElementException("Customer with ID "+customerID+" not found");
+        } else {
+            throw new NoSuchElementException("Customer with ID " + customerId + " not found");
         }
     }
 
-    public Customer getCostumerById(int customerID) {
-        boolean found = false;
-        Customer aux = new Customer(0, "n", "email@email.com", "fortnite");
-        for(int i = 0; i<customers.size(); i++){
-            if(customers.get(i).getCustomerId()==customerID){
-                aux = customers.get(i);
-                found=true;
+    public Customer getCustomerById(int customerId) {
+        for (Customer customer : customers) {
+            if (customer.getCustomerId() == customerId) {
+                return customer;
             }
         }
-        if(!found){
-            aux = null;
-        }
-        return aux;
+        return null;
     }
 
-    public void placeOrder(int customerID, List<Product> productList){
-        Customer customer= getCostumerById(customerID);
-        if(customer==null){
-            throw new NoSuchElementException("Customer with ID "+customerID+" not found");
+    public void placeOrder(int customerId, List<Product> productList) {
+        Customer customer = getCustomerById(customerId);
+        if (customer == null) {
+            throw new NoSuchElementException("Customer with ID " + customerId + " not found");
         }
 
-        int newOrderId = orders.size()+1;
+        int newOrderId = orders.size() + 1;
         Order order = new Order(newOrderId, customer);
 
-        for(Product product : productList){
+        for (Product product : productList) {
             order.addProduct(product, 1);
         }
 
@@ -137,59 +114,46 @@ public class Store {
         customer.addOrder(order);
     }
 
-    public Order getOrderById(int orderId){
-        boolean found = false;
-        int pos = 0;
-        Customer aux = new Customer(0, "n", "email@email.com", "fortnite");
-        Order order = new Order(0, aux);
-
-        while(pos<orders.size() && !found){
-            if(orders.get(pos).getOrderId()==orderId){
-                found=true;
-                order=orders.get(pos);
-            }else{
-                pos++;
+    public Order getOrderById(int orderId) {
+        for (Order order : orders) {
+            if (order.getOrderId() == orderId) {
+                return order;
             }
         }
-
-        if(!found){
-            order=null;
-        }
-
-        return order;
+        return null;
     }
 
-    public List<Order> getOrderHistoryForCustomer(int customerID){
-        Customer customer = getCostumerById(customerID);
-        if(customer!=null){
+    public List<Order> getOrderHistoryForCustomer(int customerId) {
+        Customer customer = getCustomerById(customerId);
+        if (customer != null) {
             return customer.getOrderHistory();
-        }else {
-            throw new NoSuchElementException("Customer with ID "+customerID+" not found");
+        } else {
+            throw new NoSuchElementException("Customer with ID " + customerId + " not found");
         }
     }
 
-    public void applyDiscountToProduct(int productId, double discount){
-        Product product = searchProductbyID(productId);
-        if(product!=null){
-            double newPrice = product.getPrice()-discount;
-            if(newPrice<0){
-                throw new IllegalArgumentException("Discount is too large, resulting in a negative and unvalid price");
+    public void applyDiscountToProduct(int productId, double discount) {
+        Product product = searchProductById(productId);
+        if (product != null) {
+            double newPrice = product.getPrice() - discount;
+            if (newPrice < 0) {
+                throw new IllegalArgumentException("Discount is too large, resulting in a negative and invalid price");
             }
             product.setPrice(newPrice);
-        }else{
-            throw new NoSuchElementException("Product with ID "+productId+" not found");
+        } else {
+            throw new NoSuchElementException("Product with ID " + productId + " not found");
         }
     }
 
-    public List<Product> viewAllProducts(){
+    public List<Product> viewAllProducts() {
         return new ArrayList<>(products);
     }
 
-    public List<Customer> viewAllCustomers(){
+    public List<Customer> viewAllCustomers() {
         return new ArrayList<>(customers);
     }
 
-    public List<Order> viewAllOrders(){
+    public List<Order> viewAllOrders() {
         return new ArrayList<>(orders);
     }
 }
